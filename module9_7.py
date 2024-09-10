4a75ce7138b3dfbb1ee0fb76224779703262cbba
Maximovex <maximove81@gmail.com>
2024-09-11 00:03:31 +0300

    Add files via upload

---
+++ module9_7.py
@@ -0,0 +1,33 @@
+def is_prime(func):
+    def wrapper(*args, **kwargs):
+        n = func(*args, **kwargs)
+
+        def if_prime(n):
+            if n == 2 or n == 3: return True
+            if n < 2 or n % 2 == 0: return False
+            if n < 9: return True
+            if n % 3 == 0: return False
+            r = int(n ** 0.5)
+            f = 5
+            while f <= r:
+                print('\t', f)
+                if n % f == 0: return False
+                if n % (f + 2) == 0: return False
+                f += 6
+            return True
+
+        if if_prime(n):
+            print(f'Число {n} простое')
+        else:
+            print(f'Число {n} составное')
+        return n
+
+    return wrapper
+
+
+@is_prime
+def sum_three(a, b, c):
+    return a + b + c
+
+
+print(sum_three(2, 3, 6))
